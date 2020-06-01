7a97a010cafc7be5bf9285732587e4870f1075ff
Eirik Dahlen <dahleneirik@gmail.com>
2020-06-01 19:01:17 +0200

    node2vec implementation

--- Project2/link_prediction.py
+++ Project2/link_prediction.py
@@ -1,4 +1,8 @@
 import networkx as nx
+from node2vec import Node2Vec
+from math import sqrt
+from node2vec.edges import HadamardEmbedder
+
 
 def load_graph(input_file):
     G = nx.Graph()
@@ -7,6 +11,7 @@ def load_graph(input_file):
             line = line.strip().split(" ")
             G.add_edge(line[0], line[1])
     return G
+
 
 def merge_validation_sets(filepath_pos, filepath_neg):
     validation_set = []
@@ -20,11 +25,13 @@ def merge_validation_sets(filepath_pos, filepath_neg):
             validation_set.append((line[0], line[1]))
     return validation_set
 
+
 def get_neighbors(graph):
     neighbor_dict = dict()
     for node in graph:
         neighbor_dict[node] = set(graph[node])
     return neighbor_dict
+
 
 def jaccard_similarity(validation_pairs, neighbors):
     scores = dict()
@@ -36,6 +43,7 @@ def jaccard_similarity(validation_pairs, neighbors):
         scores[author1 + ' ' + author2] = len(intersection) / len(union)
     return scores
 
+
 def adamic_adar(G, edges):
     scores = dict()
     adamic_adar_index = nx.adamic_adar_index(G, edges)
@@ -43,24 +51,57 @@ def adamic_adar(G, edges):
         scores[author1 + ' ' + author2] = index
     return scores
 
+
+def cosine_similarity(author1, author2):
+    return author1.dot(author2) / \
+           (sqrt((author1.dot(author1) * author2.dot(author2))))
+
+
+def node2vec_embedding(
+        G,
+        validation_pair,
+        walk_length=20,
+        num_walks=50,
+        p=1,
+        q=1):
+    scores = dict()
+    node2vec = Node2Vec(
+        G,
+        dimensions=128,
+        walk_length=walk_length,
+        num_walks=num_walks,
+        p=p,
+        q=q)
+    print("Fitting model...")
+    model = node2vec.fit(window=3, min_count=1, sg=1, hs=0)
+    print("Embedding...")
+    for pair in validation_pair:
+        author1 = pair[0]
+        author2 = pair[1]
+        scores[author1 + ' ' +
+               author2] = cosine_similarity(model.wv[author1], model.wv[author2])
+    return scores
+
+
 def get_sorted_top_k(results, top_k=100):
     return sorted(results.items(), key=lambda x: x[1], reverse=True)[:top_k]
+
 
 def get_reversed_pair(pair):
     pair_list = pair[0].split(" ")
     return pair_list[1] + ' ' + pair_list[0]
 
+
 def evaluate(calculated_results, ground_truth, measure=''):
     score = 0
-    truth_pairs = dict.fromkeys(pair[0] + ' ' + pair[1] for pair in ground_truth.edges())
+    truth_pairs = dict.fromkeys(pair[0] + ' ' + pair[1]
+                                for pair in ground_truth.edges())
     for pair in calculated_results:
         pair_reversed = get_reversed_pair(pair)
         if pair[0] in truth_pairs or pair_reversed in truth_pairs:
             score += 1
-    accuracy = (score/len(truth_pairs))*100
+    accuracy = (score / len(truth_pairs)) * 100
     print("The accuracy is %f %% when using measure: %s" % (accuracy, measure))
-    return accuracy
-
 
 
 def main():
@@ -71,18 +112,26 @@ def main():
              'EXAMPLE': 'data/example.txt'}
     G_training = load_graph(files['TRAINING'])
     G_val_pos = load_graph(files['VAL_POS'])
-    validation_pairs = merge_validation_sets(files['VAL_POS'], files['VAL_NEG'])
+    G_val_neg = load_graph(files['VAL_NEG'])
+    G_test = load_graph(files['TEST'])
+
+    validation_pairs = merge_validation_sets(
+        files['VAL_POS'], files['VAL_NEG'])
+
     neighbors = get_neighbors(G_training)
+
     jaccard = jaccard_similarity(validation_pairs, neighbors)
     adamic = adamic_adar(G_training, validation_pairs)
+    n2v = node2vec_embedding(G_training, validation_pairs)
+
     top_100_jaccard = get_sorted_top_k(jaccard)
     top_100_adamic = get_sorted_top_k(adamic)
-    accuracy = evaluate(top_100_jaccard, G_val_pos, 'Jaccard')
-    accuracy = evaluate(top_100_adamic, G_val_pos, 'Adamic Adar')
+    top_100_n2v = get_sorted_top_k(n2v)
+    print(top_100_n2v)
 
-
-
-
+    evaluate(top_100_jaccard, G_val_pos, 'Jaccard')
+    evaluate(top_100_adamic, G_val_pos, 'Adamic Adar')
+    evaluate(top_100_n2v, G_val_pos, 'Node2Vec')
 
 
 if __name__ == '__main__':
